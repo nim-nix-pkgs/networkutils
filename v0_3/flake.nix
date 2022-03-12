0d81097e37116570471a04b9dac450cdfc8f5311
hugosenari <hugosenari@gmail.com>
2022-03-12 04:11:17 -0300

    chore: re index networkutils

---
+++ v0_3/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Various networking utils'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-networkutils-v0_3.flake = false;
+  inputs.src-networkutils-v0_3.owner = "Q-Master";
+  inputs.src-networkutils-v0_3.ref   = "refs/tags/v0.3";
+  inputs.src-networkutils-v0_3.repo  = "networkutils.nim";
+  inputs.src-networkutils-v0_3.type  = "github";
+  
+  inputs."ptr_math".dir   = "nimpkgs/p/ptr_math";
+  inputs."ptr_math".owner = "riinr";
+  inputs."ptr_math".ref   = "flake-pinning";
+  inputs."ptr_math".repo  = "flake-nimble";
+  inputs."ptr_math".type  = "github";
+  inputs."ptr_math".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ptr_math".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-networkutils-v0_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-networkutils-v0_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
